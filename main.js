ac58eb4044da4b2b637afcd5a4cdbda91257b9aa
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-09-05 12:00:54 +0200

    basic structure

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
-});
+client.commands = new Collection();
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    const { commandName } = interaction;
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply('Server info.');
-    } else if (commandName === 'user') {
-        await interaction.reply('User info.');
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
 
 // Login to Discord with your client's token
 client.login(token);

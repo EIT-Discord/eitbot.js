013c15661ffe31b320ca1637765ec8339e00b80d
Martin Kistler <martin.kistler@hm.edu>
2021-09-05 14:19:33 +0200

    added presence command

---
+++ commands/presence.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('presence')
+        .setDescription('Set the bot`s presence!')
+        .addStringOption(option =>
+            option.setName('presence')
+                .setDescription('The new presence!')
+                .setRequired(true)),
+    async execute(interaction) {
+        const client = interaction.client;
+        const presence = interaction.options.getString('presence');
+        await client.user.setPresence({ activities: [{ name: presence }], status: 'online' })
+            .then(await interaction.reply(`Presence set to ${presence}`))
+            .catch(console.error);
+    },
+};

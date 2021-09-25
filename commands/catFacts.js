739d421bf944ce48ebe026134728d1b33e66c6b1
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-09-25 21:51:16 +0200

    Create catFacts.js

---
+++ commands/catFacts.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios').default;
+fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription("Different commands based on cats!")
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('fact')
+                .setDescription('Responses with a random cat fact!'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('gif')
+                .setDescription('Responses with a random cat fact!')),
+
+
+    async execute(interaction) {
+        const subCommand = (interaction.options.data[0]);
+
+        switch (subCommand.name)
+        {
+            case 'fact':
+                await fact(interaction);
+                break;
+        }
+
+    },
+}
+
+const fact = async interaction => {
+    axios.get('https://catfact.ninja/fact')
+        .then(async response =>  {
+            // handle success
+            await interaction.reply(response.data.fact);
+        })
+        .catch(error => {
+            // handle error
+            console.log(error);
+        })
+}
+
+

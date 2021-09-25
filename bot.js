d7a6370c106713adb1b8e8de8ee37849ad001142
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-09-25 21:51:08 +0200

    Added command deployment to bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 
 const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-const {eit} = require("./eit/classes");
-const { token } = require('./config.json');
+const { clientId, guildId, token } = require('./config.json');
 const {initParseEitConfig} = require('./eit/configParser')
-
-// Deploys all commands which may changed
-require('./deploy-commands.js');
 
 
 // Create a new client instance
 let client = new Client({
+        shards: 'auto',
         intents: [
-            Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-            Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES,
-            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS],
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.GUILD_MEMBERS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+            Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+            ],
 
         partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER']
     });
@@ -29,11 +33,18 @@ client.commands = new Collection();
 client.buttons = new Map();
 client.menus = new Map();
 
-client.eit = eit;
+client.eit = {channels: new Map(), roles: new Map(), activeSetups: new Map()};
 
+let commands = [];
 
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
+
+for (let file of eventFiles) {
+    let event = require(`./events/${file}`);
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     }
@@ -42,24 +53,41 @@ for (const file of eventFiles) {
     }
 }
 
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-}
-
-for (const file of buttonFiles) {
-    const button = require(`./buttons/${file}`);
+for (let file of buttonFiles) {
+    let button = require(`./buttons/${file}`);
     client.buttons.set(button.ID, button);
 }
 
-for (const file of menuFiles) {
-    const menu = require(`./menus/${file}`);
+for (let file of menuFiles) {
+    let menu = require(`./menus/${file}`);
     client.menus.set(menu.ID, menu);
+}
+
+// deploy slash commands
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+    try {
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands });
+        console.log('Successfully registered application commands.');
+    }
+    catch (error){
+        console.error(error);
+    }
+})();
+
+client.fetchEitMember = async () => {
+    return await rest.get(Routes.guildMembers(guildId),
+        {query: new URLSearchParams({"limit": "1000"})})
 }
 
 // Login to Discord with your client's token
 client.login(token)
 
 initParseEitConfig(client);
+
+
+
+
 

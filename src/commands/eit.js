0514e1f459db2dbc9a016e0ad283ca5f2cf26d0e
YannicBreiting <63100391+YannicBreiting@users.noreply.github.com>
2021-10-03 23:35:20 +0200

    removed files

--- src/commands/eit.js
+++ src/commands/eit.js
@@ -23,15 +23,6 @@ module.exports = {
                     option.setName('name')
                         .setDescription('Vollständiger Name!')
                         .setRequired(true)))
-
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('modmail')
-                .setDescription('Informiert die Moderatoren über dein Anliegen!')
-                .addStringOption(option =>
-                    option.setName('reason')
-                        .setDescription('Dein Anliegen!')
-                        .setRequired(true)))
         
         .addSubcommand(subcommand =>
             subcommand
@@ -50,10 +41,6 @@ module.exports = {
 
             case 'name':
                 await changeNickName(interaction);
-                break;
-
-            case 'modmail':
-                await modMail(interaction);
                 break;
 
             case 'semesterstart':
@@ -98,21 +85,6 @@ const changeNickName = async (interaction) => {
                 ephemeral: true
         })
     }
-}
-
-const modMail = async interaction => {
-    let text = interaction.options.getString('reason');
-    const moderator = interaction.client.eit.mod;
-
-    await interaction.client.fetchEitMember()
-        .then(members => members.forEach(async member => {
-            if (member.roles.includes(moderator)) {
-                await interaction.guild.members.fetch(member.user.id)
-                    .then(async member => await member.user.send(text))
-            }
-        }))
-
-    await interaction.reply({content: `Dein Anliegen wurde an die Moderatoren weitergeleitet`, ephemeral: true})
 }
 
 const semesterStart = async interaction => {

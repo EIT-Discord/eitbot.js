1485617cfe39ece102b2edbad423e691afbe833a
Martin <62301536+MartinKist@users.noreply.github.com>
2021-10-04 00:21:08 +0200

    Update setupEmbed.js

--- src/embeds/setupEmbed.js
+++ src/embeds/setupEmbed.js
@@ -4,18 +4,18 @@ const {MessageEmbed} = require("discord.js");
 module.exports = {
     start: new MessageEmbed()
         .setTitle("Herzlich Willkommen!")
-        .setDescription(`...auf unserem Elektrotechnik Discord Server! :wave:  \n\n
+        .setDescription(`...auf unserem Elektrotechnik Discord Server! :wave:\n
                         Dieses Setup ist dafür da, dass wir und deine Kommilitonen
                         dich auf dem Server (besser) erkennen und du zu deinem Semester passende 
-                        Informationen erhältst.\n\n
+                        Informationen erhältst.\n
                         *Deine Angaben werden von diesem Bot weder gespeichert noch auf irgendeine
                         Weise verarbeitet, du erhältst ledeglich deinen Namen und dein Semester 
                         auf unserem Server zugewiesen.*`),
 
     studentSelect: new MessageEmbed()
         .setTitle("Studienabschnitt")
-        .setDescription(`Es freut uns, dass ein weiterer Student auf diesen Server gefunden hat!
-                        Wähle jetzt den Studienabschnitt aus, in welchem du dich gerade befindest!`),
+        .setDescription(`Es freut uns, dass ein weiterer Student auf diesen Server gefunden hat!` + 
+                        `Wähle jetzt den Studienabschnitt aus, in welchem du dich gerade befindest!`),
 
     semesterSelect: new MessageEmbed()
         .setTitle("Semesterauswahl")
